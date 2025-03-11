29420f03fbbe7cc8990b5d437a624087e6fbc456
Nico-GR12 <nicolasgiraldoramirez3024@gmail.com>
2025-03-11 07:38:40 -0500

    Subiendo proyecto

---
+++ js/codigo_info.js
@@ -0,0 +1,149 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+const characterId = urlParams.get('id');
+
+
+console.log('ID capturado:', characterId); 
+
+console.log('ID del personaje capturado:', characterId); 
+
+const urlInfo = `https://dragonball-api.com/api/characters/${characterId}`;
+
+const opciones = {
+        method: 'GET',
+        headers: { 'accept': '*' }
+    };
+
+if (characterId) {
+    
+
+    
+
+    fetch(urlInfo, opciones)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la respuesta: ' + response.status);
+            }
+            return response.json();
+        })
+        .then(character => {
+            const infoPersonaje = document.getElementById('infoPersonaje');
+            const imagenPersonaje = document.getElementById('imagenPersonaje');
+
+            if (infoPersonaje && imagenPersonaje) {
+
+                const img = document.createElement('div');
+                img.classList.add('character-card', 'bg-secondary', 'fondo', 'mx-4', 'mt-5', 'rounded')
+
+                const imagen = document.createElement('img');
+                imagen.src = character.image
+                imagen.classList.add('img-fluid', 'imagen', 'mb-3', 'goku')
+                img.appendChild(imagen)
+                imagenPersonaje.appendChild(imagen);
+
+                const nombreTitle = document.createElement('h2')
+                nombreTitle.classList.add('card-title', 'text-white', 'mb-4');
+                nombreTitle.textContent = character.name;
+
+                const raceGender = document.createElement('h4');
+                raceGender.classList.add('card-text', 'text-warning');
+                raceGender.textContent =(`${character.race}-${character.gender}`)
+
+                const kiBase = document.createElement('h3');
+                kiBase.classList.add('card-title', 'text-white');
+                kiBase.textContent = 'ki Base';
+
+                const numberKi = document.createElement('h4');
+                numberKi.classList.add('card-text', 'text-warning');
+                numberKi.textContent = (`${character.ki}`);
+
+                const kiTotal = document.createElement('h3');
+                kiTotal.classList.add('card-title', 'text-white');
+                kiTotal.textContent = 'ki Total';
+
+                const numberKiTotal = document.createElement('h4');
+                numberKiTotal.classList.add('card-text', 'text-warning');
+                numberKiTotal.textContent = character.maxKi;
+
+                const afiliation = document.createElement('h3');
+                afiliation.classList.add('card-title', 'text-white');
+                afiliation.textContent = 'Afiliación:';
+
+                const afiliacion = document.createElement('h4');
+                afiliacion.classList.add('card-text', 'text-warning');
+                afiliacion.textContent = character.affiliation;
+                
+
+                const card = document.createElement('div');
+                card.classList.add('character-card', 'bg-secondary', 'text-center', 'mx-4', 'mt-5', 'rounded');
+
+                card.appendChild(nombreTitle)
+                card.appendChild(kiBase)
+                card.appendChild(numberKi)
+                card.appendChild(kiTotal)
+                card.appendChild(numberKiTotal)
+                card.appendChild(afiliation)
+                card.appendChild(afiliacion)
+                
+                infoPersonaje.appendChild(card)
+            } else {
+                console.error('Elementos del DOM no encontrados.');
+            }
+        })
+        .catch(error => {
+            console.error('Error al obtener los detalles del personaje:', error);
+        });
+} else {
+    console.log('No se proporcionó un ID de personaje válido.');
+};
+
+const transformaciones = document.getElementById('transformaciones')
+
+fetch(urlInfo, opciones)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la respuesta: ' + response.status);
+            }
+            return response.json();
+        })
+        .then(personaje => {
+
+            if (Array.isArray(personaje.transformations)) {
+                personaje.transformations.forEach(transformacion => {
+
+                    // const card = document.createElement("div");
+                    // card.classList.add('character-card', 'col-8', 'col-md-5', 'col-lg-3', 'fondo', 'text-center', 'mx-4', 'mt-5', 'rounded');
+                    // container.appendChild(card);
+
+                    // const img = document.createElement("img");
+                    // img.src = transformacion.image;
+                    // img.classList.add('img-fluid', 'w-75', 'goku');
+
+                    // card.appendChild(img);
+                    // transformaciones.appendChild(card);
+
+
+                    const cartaTranfor = document.createElement("div");
+                    cartaTranfor.classList.add('character-card', 'col-8', 'col-md-5', 'col-lg-3', 'carta3', 'text-center', 'mx-4', 'mt-5', 'rounded')
+                    const imgTranfor = document.createElement("img");
+                    imgTranfor.src = transformacion.image;
+                    imgTranfor.classList.add('img-fluid', 'imgTransformation', 'goku');
+
+                    const nombreTransfor = document.createElement("h2");
+                    nombreTransfor.innerHTML = transformacion.name;
+
+                    const containerInfo = document.createElement('div')
+                    containerInfo.classList.add('character-card', 'col-8', 'col-md-5', 'text-white', 'col-lg-3', 'text-center', 'mx-4', 'mt-5', 'rounded')
+                    
+                    containerInfo.appendChild(cartaTranfor)
+                    cartaTranfor.appendChild(imgTranfor);
+                    containerInfo.appendChild(nombreTransfor);
+                    transformaciones.appendChild(containerInfo);
+                });
+            } else {
+                console.error("No se encontraron transformaciones.");
+            }
+        })
+        .catch(error => {
+            console.error("Error al obtener las transformaciones:", error);
+        });
---
+++ js/mi_codigo.js
@@ -0,0 +1,182 @@
+const buscarCharacters = document.getElementById('buscarCharacters');
+
+const charactersContainer = document.getElementById('characters');
+const nameFilters = document.getElementById('nameFilters');
+const raceFilters = document.getElementById('raceFilters');
+
+
+buscarCharacters.addEventListener('click', () => {
+    getCharacters();
+});
+
+
+function getCharacters() {
+    const nameFilter = nameFilters.value;
+    const urlDragon = `https://dragonball-api.com/api/characters?name=${nameFilter}&limit=100`; 
+
+    const opciones = {
+        method: 'GET',
+        headers: {
+            'accept': '*/*'
+        }
+    };
+
+    fetch(urlDragon, opciones) 
+
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error HTTP: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayCharacters(data); 
+            console.log('Personajes', data);
+        })
+        .catch(error => {
+            console.error('Error:', error.message);
+        })
+        .finally(() => {
+            console.log('Finalizo la petición');
+        });
+}
+
+function displayCharacters(characters) {
+    charactersContainer.innerHTML = ''; 
+    for (let character of characters) {
+        const card = document.createElement('div');
+        card.classList.add('character-card', 'col-12', 'col-md-8', 'col-lg-3', 'fondo', 'text-center', 'rounded'); 
+
+        const img = document.createElement('img');
+        img.src = character.image;
+        img.classList.add('img-fluid', 'w-75', 'goku');
+
+        const nombre = document.getElementById('nombre');
+        nombre.textContent = nameFilters.value.toUpperCase()
+
+        const enlace = document.createElement('a');
+        enlace.href = `index_info.html?id=${character.id}`;
+
+        enlace.appendChild(img)
+        card.appendChild(enlace);
+        charactersContainer.appendChild(card);
+
+        console.log('ID del personaje (búsqueda):', character.id)
+    };
+    nameFilters.value = '';
+};
+
+//______________________________________________-
+
+raceFilters.addEventListener('change', ()=>{
+    filter()
+    charactersContainer.innerHTML = '';
+})
+
+function filter(){
+    const nombre = document.getElementById('nombre');
+    const race = raceFilters.value;
+    const urlfilter = `https://dragonball-api.com/api/characters?race=${race}`
+    const opciones = {
+        method: 'GET',
+        headers: {
+            'accept': '*/*'
+        }
+    };
+
+    fetch(urlfilter, opciones)
+    .then(response =>{
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+        return response.json()
+    })
+    .then(data => {
+        if (race == '' || race == 'none'){
+            charactersContainer.innerHTML = '';
+            nombre.textContent = '';
+
+        }else{
+            console.log('Respuesta de la API:', data)
+
+        const container = document.createElement("div");
+        container.classList.add('container', 'mt-4');
+
+        const row = document.createElement('div');
+        row.classList.add('row', 'justify-content-center' )
+        container.appendChild(row)
+
+        if (Array.isArray(data)) {
+
+            data.forEach(data => {
+
+                const card = document.createElement("div");
+                card.classList.add('character-card', 'col-8', 'col-md-5', 'col-lg-3', 'fondo', 'text-center', 'mx-4', 'mt-5', 'rounded');
+                container.appendChild(card);
+
+                nombre.textContent =  race.toUpperCase()
+
+                const img = document.createElement("img");
+                img.src = data.image;
+                img.classList.add('img-fluid', 'w-75', 'goku');
+
+                const enlace = document.createElement('a');
+                enlace.href =`index_info.html?id=${data.id}`;
+
+                enlace.appendChild(img)
+                card.appendChild(enlace);
+                row.appendChild(card);
+                charactersContainer.appendChild(container);
+            })
+            console.log('ID del personaje (búsqueda):', data.id)
+
+        }else{
+            console.log("No hay personajes");
+        }            
+        console.log('personajes', data)
+        }
+    })
+    .catch(error => {
+        console.error('Error al obtener los datos:', error);
+    });
+}
+//______________________________________________________________________________
+
+const link = (`https://dragonball-api.com/api/characters?page=1&limit=58`);
+
+const respuesta = {
+    method: 'GET',
+    headers: { 'accept': '/' }
+};
+
+fetch(link, respuesta)
+    .then(response => {
+        return response.json();
+    })
+    .then(personaje => {
+        const contenedor = document.getElementById("personajes");
+
+        if (Array.isArray(personaje.items)) {
+            
+            personaje.items.forEach(personaje => {
+
+                const card = document.createElement("div");
+                card.classList.add('character-card', 'col-8', 'col-md-5', 'col-lg-3', 'fondo', 'text-center', 'mx-4', 'mt-5', 'rounded');
+                
+
+                const img = document.createElement("img");
+                img.src = personaje.image;
+                img.classList.add('img-fluid', 'goku');
+
+                const enlace = document.createElement('a');
+                enlace.href = `index_info.html?id=${personaje.id}`;
+
+                enlace.appendChild(img)
+                card.appendChild(enlace);
+                contenedor.appendChild(card);
+            })
+        }else{
+            console.log("No hay personajes");
+        } 
+    })
+
